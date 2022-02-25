a937b73461cf7ca44cf7447d2c949683609e98b0
keza681 <lindakeza19@gmail.com>
2022-02-26 00:12:25 +0200

    Add interactive list to the todo

---
+++ src/modules/functionality.js
@@ -0,0 +1,10 @@
+const setState = (tasks, checkbox, index) => {
+    if (checkbox.checked === true) {
+      tasks[index].checked = true;
+    } else {
+      tasks[index].checked = false;
+    }
+  };
+  
+  export default setState;
+  
--- src/modules/getStates.js
+++
@@ -1,9 +0,0 @@
-const setState = (tasks, checkbox, index) => {
-  if (checkbox.checked === true) {
-    tasks[index].checked = true;
-  } else {
-    tasks[index].checked = false;
-  }
-};
-
-export default setState;
